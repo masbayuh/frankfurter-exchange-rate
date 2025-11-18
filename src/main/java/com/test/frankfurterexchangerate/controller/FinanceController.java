9db8baa6d25bdf09622867d22fbecaa85d079120
masbayuh <59537202+masbayuh@users.noreply.github.com>
2025-11-18 09:53:06 +0700

    Finance Frankfurter Exchange rates

---
+++ src/main/java/com/test/frankfurterexchangerate/controller/FinanceController.java
@@ -0,0 +1,27 @@
+package com.test.frankfurterexchangerate.controller;
+
+import com.test.frankfurterexchangerate.service.FinanceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/finance/data")
+public class FinanceController {
+    private final FinanceService financeService;
+
+    public FinanceController(FinanceService financeService) {
+        this.financeService = financeService;
+    }
+
+    @GetMapping("/{resourceType}")
+    public ResponseEntity<List<Map<String, Object>>> getData(@PathVariable String resourceType){
+        List<Map<String, Object>> result = financeService.fetchResource(resourceType);
+        return ResponseEntity.ok(result);
+    }
+}
